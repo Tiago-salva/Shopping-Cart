45943e012750a6f2244fdece67b4ce8d6a722d5a
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-06 21:02:40 -0300

    add a page to show all the products (in the future)

---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Products() {
+  return (
+    <>
+      <Header />
+      <h1>Estos son los productos</h1>
+      <Footer />
+    </>
+  );
+}
