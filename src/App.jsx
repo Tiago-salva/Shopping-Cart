5e26caf9bbae88319eaacd540582ed3e45f27e15
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-09 17:16:30 -0300

    remove the header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <h1>Esto es el home page</h1>
-      <Footer />
     </>
   );
 }
