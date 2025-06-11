94a65ccbe89e7eaa7ffb9aaaaaf73149f78b1e2c
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-11 15:49:35 -0300

    add btns to add the product to cart and navigate to cart

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,13 +1,16 @@
-import { useParams } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import styles from "./ProductDetail.module.css";
 
 export default function ProductDetail() {
   const { id } = useParams();
   const [selectedProduct, setSelectedProduct] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log(selectedProduct);
+  }, [selectedProduct]);
 
   useEffect(() => {
     setLoading(true);
@@ -24,13 +27,20 @@ export default function ProductDetail() {
   if (loading) return <p>Cargando producto...</p>;
 
   return (
-    selectedProduct && (
+    Object.keys(selectedProduct).length > 0 && (
       <>
-        <div>
-          <h1>{selectedProduct.title}</h1>
-          <img src={selectedProduct.image}></img>
-          <p>${selectedProduct.price}</p>
-          <p>{selectedProduct.description}</p>
+        <div className={styles.selected_product_container}>
+          <div className={styles.selected_product_img}>
+            <img src={selectedProduct.image}></img>
+            <p>{selectedProduct.description}</p>
+          </div>
+          <aside className={styles.selected_product_info}>
+            <h1>{selectedProduct.title}</h1>
+            <p>Calificacion {selectedProduct.rating.rate} </p>
+            <p>${selectedProduct.price}</p>
+            <button>Agregar al carrito</button>
+            <Link to="/checkout">Comprar</Link>
+          </aside>
         </div>
       </>
     )
