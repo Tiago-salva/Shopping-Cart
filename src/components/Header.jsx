de4312cdc1cb4ebf86bf3f2476db14d8ec140669
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-11 15:34:13 -0300

    add functionality to the search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [inputValue, setInputValue] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const filterProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue.toLowerCase());
+  });
+
   return (
     <header>
+      {filterProducts.length > 0 && inputValue !== "" && (
+        <div className="searched_products">
+          {filterProducts.map((product, index) => (
+            <Link to={`/products/${product.id}`} key={index}>
+              <p>{product.title}</p>
+            </Link>
+          ))}
+        </div>
+      )}
       <Link to="/">Titulo</Link>
-      <input type="text" placeholder="Buscar"></input>
+      <input
+        type="text"
+        placeholder="Buscar"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
       <nav>
         <ul>
           <li>
