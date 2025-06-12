65737ba128e2198f413fa6caa8fe52c3bb1ec518
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-12 17:47:43 -0300

    show the products in the cart and the total amount

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,3 +1,39 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import styles from "./Checkout.module.css";
+
 export default function Checkout() {
-  return <></>;
+  const { cartItems, addQuantity, removeFromCart } = useContext(CartContext);
+  const cartItemsTotalPrice = cartItems.reduce((accumulator, item) => {
+    return accumulator + item.price * item.quantity;
+  }, 0);
+
+  return (
+    <>
+      {Object.keys(cartItems).length > 0 ? (
+        <>
+          <div className={styles.cart_items_container}>
+            {cartItems.map((product) => (
+              <div key={product.id}>
+                <img src={product.image}></img>
+                <h5>{product.title}</h5>
+                <p>$ {product.price}</p>
+                <div className={styles.cart_items_quantity}>
+                  <button onClick={() => removeFromCart(product)}>-</button>
+                  <p>{product.quantity}</p>
+                  <button onClick={() => addQuantity(product)}>+</button>
+                </div>
+                <p>Total: $ {product.price * product.quantity}</p>
+              </div>
+            ))}
+          </div>
+          <div className={styles.cart_items_total}>
+            <p>Total: {Math.round(cartItemsTotalPrice * 100) / 100}</p>
+          </div>
+        </>
+      ) : (
+        <h1>No tienes productos en tu carrito</h1>
+      )}
+    </>
+  );
 }
