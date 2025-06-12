56a5b8a341ea7ef4388921d138e1ee7f0f18e3bf
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-06-12 17:46:27 -0300

    wrap the pages with shopping cart context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>
 );
